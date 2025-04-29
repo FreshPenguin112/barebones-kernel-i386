d25a2fd685bd84d1e13036f7ba52951f7b8a57b1
FreshPenguin112 <93781766+FreshPenguin112@users.noreply.github.com>
2025-04-29 17:35:40 +0000

    remove stuff

--- build2.sh
+++ build2.sh
@@ -1,5 +1,7 @@
-sudo rm *.o
-#sudo rm initfs/*.elf
+rm -f *.o
+rm -f initfs/*.elf
+sleep 1
 docker run --mount type=bind,source=$(realpath .),target=/root -w /root kevincharm/i686-elf-gcc-toolchain:5.5.0 sh build.sh
+sleep 1
 tar -cf initfs.tar -C initfs .
 xxd -i initfs.tar > initramfs.c
