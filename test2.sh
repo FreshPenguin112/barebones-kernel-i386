7e8ac136abca556c7ce8341c57dfeab7205cc900
FreshPenguin112 <freshpenguin112@gmail.com>
2025-08-29 11:18:41 -0500

    small windows fix

--- test2.sh
+++ test2.sh
@@ -1,1 +1,1 @@
-qemu/qemu-system-i386 -kernel mykernel.elf -device isa-debug-exit,iobase=0x501,iosize=0x04 -no-reboot -display default,show-cursor=on
+qemu/qemu-system-i386 -kernel mykernel.elf -device isa-debug-exit,iobase=0x501,iosize=0x04 -no-reboot -display default,show-cursor=on -serial stdio
